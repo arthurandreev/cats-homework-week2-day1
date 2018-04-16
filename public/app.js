69c03b0aa42f5d684feb8f3d9d66d2430c20f25b
Arthur Andreev <arthur.mailbox@yandex.com>
2018-04-16 22:17:04 +0100

    MVP completed

--- public/app.js
+++ public/app.js
@@ -1,44 +1,74 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//
+// const cats = document.querySelector('#cats');
+//
+// cat.innerHTML= '';
+//
+// const createCat = function(name, food, imageSrc, container){
+//
+//
+//   });
+//   const ulItem = document.createElement('ul');
+//
+//   const listItem = document.createElement('li');
+//
+//   const catName = document.createElement('li');
+//   catName.textContent = 'Name: Steve';
+//
+//   const catFood = document.createElement('li');
+//
+//   catFood.textContent = 'Favourite food: beans on toast';
+//
+//   const catImage = document.createElement('img');
+//   catImage.src = 'http://littlefun.org/uploads/521d1a62c8561116e1000000_736.jpg';
+//   catImage.width = 500;
+//
+//   cats.appendChild(ulItem);
+//   ulItem.appendChild(catName);
+//   ulItem.appendChild(catFood);
+//   ulItem.appendChild(listItem);
+//   listItem.appendChild(catImage);
+
+
+// });
+
+//refactored above code, turned it into a method to add cats to the DOM
 document.addEventListener('DOMContentLoaded', () => {
 
   const cats = document.querySelector('#cats');
+  cats.innerHTML = '';
 
-  const ulItem = document.createElement('ul');
+  const createNewCat = function (cat) {
 
-  const listItem = document.createElement('li');
+    const ulNewCatItem = document.createElement('ul');
 
-  const catName = document.createElement('li');
-  catName.textContent = 'Name: Steve';
+    const liCatName = document.createElement('li');
+    liCatName.textContent = `Name: ${cat[0]}`;
 
-  const catFood = document.createElement('li');
+    const liCatFavouriteFood = document.createElement('li');
+    liCatFavouriteFood.textContent = `Favourite food: ${cat[1]}`;
 
-  catFood.textContent = 'Favourite food: beans on toast';
+    const liNewCatImage = document.createElement('li');
+    const image = document.createElement('img');
+    image.width = 500;
+    image.src = cat[2];
 
-  const catImage = document.createElement('img');
-  catImage.src = 'http://littlefun.org/uploads/521d1a62c8561116e1000000_736.jpg';
-  catImage.width = 500;
+    cats.appendChild(ulNewCatItem);
+    ulNewCatItem.appendChild(liCatName);
+    ulNewCatItem.appendChild(liCatFavouriteFood);
+    ulNewCatItem.appendChild(liNewCatImage);
+    liNewCatImage.appendChild(image);
+  }
 
-  cats.appendChild(ulItem);
-  ulItem.appendChild(catName);
-  ulItem.appendChild(catFood);
-  ulItem.appendChild(listItem);
-  listItem.appendChild(catImage);
+const boba = ['Boba', 'Sock fluff', 'http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg']
+const barnaby = ['Barnaby', 'Tuna', 'https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg']
+const max = ['Max', 'Whiskas Temptations', 'http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg']
+const superCat = ['SuperCat', 'Fish and Chips', 'http://littlefun.org/uploads/521d1a62c8561116e1000000_736.jpg']
 
-const addAnImage = function(ulItem, catName, catFood, listItem, catImage){
+const catArray = [superCat, boba, max, barnaby];
 
+for (let i = 0; i < catArray.length; i++) {
+  createNewCat(catArray[i]);
 }
-  // //another way of doing it
-  // const container = document.querySelector('.container');
-  // //innerHTML refers to HTML inside the container
-  // //container is cleared out by assinging it to an empty string
-  // container.innerHTML = '';
-  //
-  // const createImage = function(src, container){
-  //     const image = document.createElement('img');
-  //     image.src = src;
-  //     container.appendChild(image);
-  // }
-  //
-  // for(let i = 1; i >= 15; i++){
-  //   createImage(`images/IMG${i}.JPG`, container);
-  // }
+
 });
